f76d52b39102154aab7a88dcc5db72b642de0219
Justin Wang <justinwang46@gmail.com>
2023-04-27 20:00:40 -0400

    include u and v in hit record

--- include/hittable.h
+++ include/hittable.h
@@ -10,6 +10,8 @@ struct hit_record {
     vec3 normal;
     shared_ptr<material> mat_ptr;
     float t;
+    float u;
+    float v;
     bool front_face;
 
     inline void set_face_normal(const ray& r, const vec3& outward_normal) {
